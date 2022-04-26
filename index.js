dadee1d91f7103f196ba066145f99c7beffc22f5
Chris <chrisdietrich3666@gmail.com>
2022-04-25 21:52:18 -0500

    added database using Heidisql

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
 const app = express();
+const db = require("./config/db");
+db.sync()
+  .then(() => console.log("db connection successful"))
+  .catch((e) => console.log(e));
 app.use(cors());
 const bodyParser = require("body-parser");
 
@@ -12,4 +16,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", routes());
 
-app.listen(3000);
+app.listen(3001);
